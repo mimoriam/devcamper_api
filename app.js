f0b4e61d0d7e96176b8fd1f34c357ffba55df86d
Mimoriam <mimoriam420@gmail.com>
2021-08-11 01:10:57 +0500

    Commit #2 - Created controllers

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const path = require('path');
 const dotenv = require('dotenv')
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 // Load env vars:
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const bootcampRouter = require('./routes/bootcamps');
 
 const app = express();
 
@@ -18,7 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/bootcamps', bootcampRouter);
 
 module.exports = app;
---
+++ controllers/bootcamps.js
@@ -0,0 +1,34 @@
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = (req, res, next) => {
+
+}
+
+// @desc    Get single bootcamps
+// @route   GET /api/v1/bootcamps/:id
+// @access  Public
+exports.getBootcamp = (req, res, next) => {
+
+}
+
+// @desc    Create new bootcamp
+// @route   POST /api/v1/bootcamps
+// @access  Private (Authentication required)
+exports.createBootcamp = (req, res, next) => {
+
+}
+
+// @desc    Update a bootcamp
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private (Authentication required)
+exports.updateBootcamp = (req, res, next) => {
+
+}
+
+// @desc    Delete a bootcamp
+// @route   DELETE /api/v1/bootcamps/:id
+// @access  Private (Authentication required)
+exports.deleteBootcamp = (req, res, next) => {
+
+}
